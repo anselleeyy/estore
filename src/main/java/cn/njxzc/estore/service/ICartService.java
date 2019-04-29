5eb4786634cf8b95a858948d836f3dd7f7d401e1
anselleeyy <573459407@qq.com>
2019-04-30 01:09:23 +0800

    完成部分购物车逻辑开发

--- src/main/java/cn/njxzc/estore/service/ICartService.java
+++ src/main/java/cn/njxzc/estore/service/ICartService.java
@@ -6,6 +6,11 @@ import cn.njxzc.estore.entity.Cart;
 
 public interface ICartService {
 
+	/**
+	 * 根据 userId 查询用户的所有购物车商品信息(status=1)
+	 * @param userId
+	 * @return
+	 */
 	public List<Cart> findAllCarts(Long userId);
 
 	public boolean delete(int id);
